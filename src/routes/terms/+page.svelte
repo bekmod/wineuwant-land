69274dbdae605a44663592d5b89b436a5357c1f0
Kiryl Budnik <kirill.budnik@hotmail.com>
2026-04-05 21:28:01 +0300

    feat: add Cookies, Privacy, and Terms pages with layout and structure

---
+++ src/routes/terms/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Footer from '$lib/Footer.svelte';
+  import NavBar from '$lib/NavBar.svelte';
+  import Roots from '$lib/Roots.svelte';
+</script>
+
+<NavBar />
+
+<section>
+  <div class="container mx-auto py-24 px-8">
+    <h2 class="text-center text-3xl text-burgundy-400">Terms of Use</h2>
+  </div>
+</section>
+
+<Footer />
